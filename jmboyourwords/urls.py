db4909f2a257427dbb1a22d93121db5f65f7e0ec
Milton Madanda <milton@praekeltfoundation.org>
2012-05-26 12:06:37 +0200

    changed story url

--- jmboyourwords/urls.py
+++ jmboyourwords/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('jmboyourwords.views',
-    url(r'^your_story/(?P<competition_id>\d+)/$',\
+    url(r'^(?P<competition_id>\d+)/$',\
         'your_story',\
         name='your_story'),
 )
