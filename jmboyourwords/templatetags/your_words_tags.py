5018d0e9c75dbc0dbc2620da05ec947e37c6b99f
Milton <milton@praekeltfoundation.org>
2012-11-07 17:11:58 +0200

    fix stories publish to selected site only

--- jmboyourwords/templatetags/your_words_tags.py
+++ jmboyourwords/templatetags/your_words_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from jmboyourwords.models import YourStoryCompetition
+from django.contrib.sites.models import Site
 
 register = template.Library()
 
@@ -7,6 +8,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def get_your_words_competitions(context, limit=5, var_name='your_words_list'):
     stories = YourStoryCompetition.objects.filter(published=True)\
+                                          .filter(site=Site.objects.get_current())\
                                           .order_by('-publish_on')
     context[var_name] = stories
     return ""
